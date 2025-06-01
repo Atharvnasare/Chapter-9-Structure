a876ca67dbd2353b9cfdb1c440c363bbcce3ef61
Atharvnasare <nasareatharv2003@gmail.com>
2025-06-01 23:28:56 +0530

    new commit 3

--- nine.c
+++ nine.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 
 //User defined datatype
 struct student{
@@ -8,6 +9,16 @@ struct student{
 };
 
 int main (){
+    struct student s1;
+
+    s1.roll =100;
+    // s1.name = "Atharv";
+    s1.cgpa = 9.3;
+    strcpy(s1.name , "Atharv Shamdev Nasare");
+
+    printf("Student name : %s \n" , s1.name);
+        printf("Student roll no : %d \n" , s1.roll);
+                printf("Student cgpa : %f \n" , s1.cgpa);
 
     return 0;
 }
