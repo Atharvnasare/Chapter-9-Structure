a9db677bb02ea48a21349f4393a278289ccf20c5
Atharvnasare <nasareatharv2003@gmail.com>
2025-06-03 14:38:22 +0530

    new commit 3

--- nine.c
+++ nine.c
@@ -26,49 +26,20 @@
 /*Write a program to store the data of 3 students*/
 
 //User defined datatype
+// 
 struct student{
     int roll ;
-    float cgpa ;
+    float cgpa;
     char name[100];
 };
 
-int main (){
-    struct student s1;
+int main(){
+    struct student ece[100];
+    ece[0].roll =1001;
+    ece[0].cgpa =9.5;
+    strcpy(ece[0].name , "Atharv");
 
-    s1.roll =100;
-    // s1.name = "Atharv";
-    s1.cgpa = 9.3;
-    strcpy(s1.name , "Atharv Shamdev Nasare");
-
-    printf("Student name : %s \n" , s1.name);
-    printf("Student roll : %d \n" , s1.roll);
-    printf("Student cgpa : %f \n" , s1.cgpa);
-        printf("\n");
-
-    struct student s2;
-
-    s2.roll = 101;
-    s2.cgpa = 9.4;
-    strcpy(s2.name ,   "Aman Surkar");
-
-    printf("Student name : %s \n" , s2.name);
-    printf("Student roll : %d \n" , s2.roll);
-    printf("Student cgpa : %f \n" , s2.cgpa);
-        printf("\n");
-
-
-    struct student s3;
-
-    s3.roll = 102;
-    s3.cgpa = 9.5;
-    strcpy(s3.name , "Ashwini Rewatkar");
-
-    printf("Student name : %s \n" , s3.name);
-    printf("Student roll : %d \n" , s3.roll);
-    printf("Student cgpa : %f \n" , s3.cgpa);
-            printf("\n");
-
-
-    return 0;
+    printf("name = %s \n" , ece[0].name );
+    printf("roll no :%d \n" , ece[0].roll);
+    printf("Cgpa = %f \n" , ece[0].cgpa);
 }
-
